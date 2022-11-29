4eb43cdfb85db44521bd5ce683c9e9051136ca33
Yaren Ulusoy <ulusoyyaren@gmail.com>
2022-11-29 15:20:53 +0300

    Create artikyil.java

---
+++ artikyil.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class ArtikYil {
+
+    public static void main(String[] args) {
+
+            int year;
+
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Lütfen Yıl Giriniz : ");
+            year = input.nextInt();
+
+
+            if(year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(year + " bir artık yıldır!");
+                }else{
+                    System.out.println(year + " bir artık yıl değildir!");
+                }
+            } else  if (year % 4 == 0) {
+                System.out.println(year + " bir artık yıldır!");
+            } else {
+                System.out.println(year + " bir artık yıl değildir!");
+            }
+
+        }
+    }
